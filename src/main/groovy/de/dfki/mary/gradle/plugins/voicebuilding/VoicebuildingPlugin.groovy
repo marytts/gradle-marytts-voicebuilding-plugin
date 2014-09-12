e0f534edac9efd85f86a436ddf55bded96020fd7
Ingmar Steiner <steiner@coli.uni-saarland.de>
2014-09-12 14:14:22 +0200

    outsource extension classes

--- src/main/groovy/de/dfki/mary/gradle/plugins/voicebuilding/VoicebuildingPlugin.groovy
+++ src/main/groovy/de/dfki/mary/gradle/plugins/voicebuilding/VoicebuildingPlugin.groovy
@@ -10,58 +10,6 @@ import org.gradle.api.tasks.bundling.Zip
 
 import org.apache.commons.codec.digest.DigestUtils
 
-class VoicebuildingPluginVoiceExtension {
-    final Project project
-
-    def description
-    def gender
-    def name
-    def nameCamelCase
-    def language
-    def locale
-    def localeXml
-    def maryLocale
-    def maryLocaleXml
-    def region
-    def samplingRate
-    def type
-    def wantsToBeDefault
-
-    VoicebuildingPluginVoiceExtension(final Project project) {
-        this.project = project
-    }
-
-    def getLocale() {
-        locale = locale ?: [language, getRegion()].join('_')
-    }
-
-    def getLocaleXml() {
-        localeXml = localeXml ?: [language, getRegion()].join('-')
-    }
-
-    def getMaryLocale() {
-        maryLocale = maryLocale ?: language.equalsIgnoreCase(getRegion()) ? language : getLocale()
-    }
-
-    def getMaryLocaleXml() {
-        maryLocaleXml = maryLocaleXml ?: language.equalsIgnoreCase(getRegion()) ? language : getLocaleXml()
-    }
-
-    def getNameCamelCase() {
-        nameCamelCase = nameCamelCase ?: name.split(/[^_A-Za-z0-9]/).collect { it.capitalize() }.join()
-    }
-
-    def getRegion() {
-        region = region ?: language.toUpperCase()
-    }
-}
-
-class VoicebuildingPluginLicenseExtension {
-    def name
-    def shortName
-    def url
-}
-
 class VoicebuildingPlugin implements Plugin<Project> {
     final templateDir = "/de/dfki/mary/gradle/plugins/voicebuilding/templates"
     def voice
---
+++ src/main/groovy/de/dfki/mary/gradle/plugins/voicebuilding/VoicebuildingPluginLicenseExtension.groovy
@@ -0,0 +1,7 @@
+package de.dfki.mary.gradle.plugins.voicebuilding
+
+class VoicebuildingPluginLicenseExtension {
+    def name
+    def shortName
+    def url
+}
