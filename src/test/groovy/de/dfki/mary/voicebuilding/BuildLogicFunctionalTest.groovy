044637091197d4c75539cb372c281d3aba49c956
Ingmar Steiner <steiner@coli.uni-saarland.de>
2015-09-01 21:51:21 +0200

    use gradle testkit for functional testing

--- src/test/groovy/de/dfki/mary/voicebuilding/BuildLogicFunctionalTest.groovy
+++ src/test/groovy/de/dfki/mary/voicebuilding/BuildLogicFunctionalTest.groovy
@@ -1,12 +1,25 @@
 package de.dfki.mary.voicebuilding
 
+import org.gradle.testkit.runner.GradleRunner
 import org.testng.annotations.*
+
+import java.nio.file.Files
+
+import static org.gradle.testkit.runner.TaskOutcome.*
 
 class BuildLogicFunctionalTest {
 
-    @Test
-    void fail() {
-        assert false
+    def gradle
+
+    @BeforeSuite
+    void setup() {
+        def projectDir = Files.createTempDirectory(null).toFile()
+        gradle = GradleRunner.create().withProjectDir(projectDir)
     }
 
+    @Test
+    void testBuild() {
+        def result = gradle.build()
+        assert result.task(':help').outcome == SUCCESS
+    }
 }
