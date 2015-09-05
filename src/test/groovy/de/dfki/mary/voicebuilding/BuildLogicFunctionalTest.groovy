a4aba121681e334663c10df67cea4bfd5799a999
Ingmar Steiner <steiner@coli.uni-saarland.de>
2015-09-05 17:51:14 +0200

    fix bug with offline property parsing
    "false" is not the empty string, and evaluates to true if just cast to boolean

--- src/test/groovy/de/dfki/mary/voicebuilding/BuildLogicFunctionalTest.groovy
+++ src/test/groovy/de/dfki/mary/voicebuilding/BuildLogicFunctionalTest.groovy
@@ -18,7 +18,7 @@ class BuildLogicFunctionalTest {
         def projectDir = new File(System.properties.testProjectDir)
         projectDir.mkdirs()
         gradle = GradleRunner.create().withProjectDir(projectDir)
-        if (System.properties.offline as boolean) {
+        if (System.properties.offline.toBoolean()) {
             gradle = gradle.withArguments('--offline')
         }
         buildFile = new File(projectDir, 'build.gradle')
