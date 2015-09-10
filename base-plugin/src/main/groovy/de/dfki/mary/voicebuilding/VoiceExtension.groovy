81e589de5bae1ad3515c23d93ba6426e3ef04488
Ingmar Steiner <steiner@coli.uni-saarland.de>
2015-09-10 15:49:09 +0200

    move default field values into VoiceExtension constructor

--- base-plugin/src/main/groovy/de/dfki/mary/voicebuilding/VoiceExtension.groovy
+++ base-plugin/src/main/groovy/de/dfki/mary/voicebuilding/VoiceExtension.groovy
@@ -1,13 +1,24 @@
 package de.dfki.mary.voicebuilding
 
 class VoiceExtension {
-    String name = 'my_voice'
-    String gender = 'female'
-    String domain = 'general'
-    String language = 'en'
-    String region = 'US'
-    String type = 'unit selection'
-    int samplingRate = 16000
+
+    String name
+    String gender
+    String domain
+    String language
+    String region
+    String type
+    int samplingRate
+
+    VoiceExtension() {
+        name = 'my_voice'
+        gender = 'female'
+        domain = 'general'
+        language = 'en'
+        region = 'US'
+        type = 'unit selection'
+        samplingRate = 16000
+    }
 
     def nameCamelCase = {
         name?.split(/[^_A-Za-z0-9]/).collect { it.capitalize() }.join()
