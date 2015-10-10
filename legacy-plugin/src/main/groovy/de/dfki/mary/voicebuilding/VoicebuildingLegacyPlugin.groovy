c1b7a7522bb4a2027eea1caee967d9478efcfb29
Ingmar Steiner <steiner@coli.uni-saarland.de>
2015-10-10 08:07:51 +0200

    add stub legacy-plugin

---
+++ legacy-plugin/src/main/groovy/de/dfki/mary/voicebuilding/VoicebuildingLegacyPlugin.groovy
@@ -0,0 +1,11 @@
+package de.dfki.mary.voicebuilding
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+class VoicebuildingLegacyPlugin implements Plugin<Project> {
+
+    @Override
+    void apply(Project project) {
+    }
+}
