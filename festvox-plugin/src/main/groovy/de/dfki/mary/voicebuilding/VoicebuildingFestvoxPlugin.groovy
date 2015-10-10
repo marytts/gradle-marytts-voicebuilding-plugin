cee300e15b13312c4825c0baf3c2bb1407d6295a
Ingmar Steiner <steiner@coli.uni-saarland.de>
2015-10-10 08:07:51 +0200

    add stub festvox plugin

---
+++ festvox-plugin/src/main/groovy/de/dfki/mary/voicebuilding/VoicebuildingFestvoxPlugin.groovy
@@ -0,0 +1,13 @@
+package de.dfki.mary.voicebuilding
+
+import de.dfki.mary.voicebuilding.tasks.*
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+class VoicebuildingFestvoxPlugin implements Plugin<Project> {
+
+    @Override
+    void apply(Project project) {
+    }
+}
