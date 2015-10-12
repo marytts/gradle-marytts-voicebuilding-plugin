c43c33b2bbff5b3611fc7ab643aca92cc9c7c11d
Ingmar Steiner <steiner@coli.uni-saarland.de>
2015-10-12 15:55:15 +0200

    add stub functional test for festvox-plugin

---
+++ festvox-plugin/src/test/groovy/de/dfki/mary/voicebuilding/BuildLogicFunctionalTest.groovy
@@ -0,0 +1,44 @@
+package de.dfki.mary.voicebuilding
+
+import org.gradle.testkit.runner.GradleRunner
+import org.testng.annotations.*
+
+import static org.gradle.testkit.runner.TaskOutcome.*
+
+class BuildLogicFunctionalTest {
+
+    def gradle
+    def buildFile
+
+    @BeforeSuite
+    void setup() {
+        def projectDir = new File(System.properties.testProjectDir)
+        projectDir.mkdirs()
+        buildFile = new File(projectDir, 'build.gradle')
+
+        def pluginClasspathResource = getClass().classLoader.findResource("plugin-classpath.txt")
+        if (pluginClasspathResource == null) {
+            throw new IllegalStateException("Did not find plugin classpath resource, run `testClasses` build task.")
+        }
+
+        def pluginClasspath = pluginClasspathResource.readLines()
+                .collect { it.replace('\\', '\\\\') } // escape backslashes in Windows paths
+                .collect { new File(it) }
+
+        gradle = GradleRunner.create().withProjectDir(projectDir).withPluginClasspath(pluginClasspath)
+
+        // Add the logic under test to the test build
+        buildFile << """
+        plugins {
+            id 'de.dfki.mary.voicebuilding-festvox'
+        }
+        """
+    }
+
+    @Test
+    void testBuild() {
+        def result = gradle.build()
+        println result.standardOutput
+        assert result.task(':help').outcome == SUCCESS
+    }
+}
