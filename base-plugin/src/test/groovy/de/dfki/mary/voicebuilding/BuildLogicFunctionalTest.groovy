4ab1c86b73f38d1c9557eb226eda2bba8c28453c
Ingmar Steiner <steiner@coli.uni-saarland.de>
2015-10-13 11:01:20 +0200

    add stub functional test for base-plugin

---
+++ base-plugin/src/test/groovy/de/dfki/mary/voicebuilding/BuildLogicFunctionalTest.groovy
@@ -0,0 +1,55 @@
+package de.dfki.mary.voicebuilding
+
+import org.gradle.testkit.runner.GradleRunner
+import org.testng.annotations.*
+
+import static org.gradle.testkit.runner.TaskOutcome.*
+
+class BuildLogicFunctionalTest {
+
+    def gradle
+    def buildFile
+
+    @BeforeSuite
+    void setup() {
+        def projectDir = new File(System.properties.testProjectDir)
+        projectDir.mkdirs()
+        buildFile = new File(projectDir, 'build.gradle')
+
+        def pluginClasspathResource = getClass().classLoader.findResource("plugin-classpath.txt")
+        if (pluginClasspathResource == null) {
+            throw new IllegalStateException("Did not find plugin classpath resource, run `testClasses` build task.")
+        }
+
+        def pluginClasspath = pluginClasspathResource.readLines()
+                .collect { it.replace('\\', '\\\\') } // escape backslashes in Windows paths
+                .collect { new File(it) }
+
+        gradle = GradleRunner.create().withProjectDir(projectDir).withPluginClasspath(pluginClasspath)
+
+        // Add the logic under test to the test build
+        buildFile << """
+        plugins {
+            id 'de.dfki.mary.voicebuilding-base'
+        }
+
+        task testPlugins(group: 'Verification') << {
+            assert plugins.findPlugin('de.dfki.mary.voicebuilding-base')
+        }
+        """
+    }
+
+    @Test
+    void testHelp() {
+        def result = gradle.build()
+        println result.standardOutput
+        assert result.task(':help').outcome == SUCCESS
+    }
+
+    @Test
+    void testPlugins() {
+        def result = gradle.withArguments('testPlugins').build()
+        println result.standardOutput
+        assert result.task(':testPlugins').outcome == SUCCESS
+    }
+}
