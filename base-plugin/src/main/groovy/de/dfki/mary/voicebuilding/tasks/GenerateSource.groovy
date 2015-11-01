e5dfaf701a1721fb5ff3bd485b3c7f10fffc51a9
Ingmar Steiner <steiner@coli.uni-saarland.de>
2015-11-01 21:31:44 +0100

    tweak indentation

--- base-plugin/src/main/groovy/de/dfki/mary/voicebuilding/tasks/GenerateSource.groovy
+++ base-plugin/src/main/groovy/de/dfki/mary/voicebuilding/tasks/GenerateSource.groovy
@@ -19,17 +19,19 @@ class GenerateSource extends DefaultTask {
                     marytts {
                         voice {
                             "${project.voice.nameCamelCase}" {
-                                'Config.java' """|package marytts.voice.${project.voice.nameCamelCase};
-                                                 |
-                                                 |import marytts.config.VoiceConfig;
-                                                 |import marytts.exceptions.MaryConfigurationException;
-                                                 |
-                                                 |public class Config extends VoiceConfig {
-                                                 |    public Config() throws MaryConfigurationException {
-                                                 |        super(Config.class.getResourceAsStream("voice.config"));
-                                                 |    }
-                                                 |}
-                                                 |""".stripMargin()
+                                'Config.java'(
+                                        """|package marytts.voice.${project.voice.nameCamelCase};
+                                           |
+                                           |import marytts.config.VoiceConfig;
+                                           |import marytts.exceptions.MaryConfigurationException;
+                                           |
+                                           |public class Config extends VoiceConfig {
+                                           |    public Config() throws MaryConfigurationException {
+                                           |        super(Config.class.getResourceAsStream("voice.config"));
+                                           |    }
+                                           |}
+                                           |""".stripMargin()
+                                )
                             }
                         }
                     }
