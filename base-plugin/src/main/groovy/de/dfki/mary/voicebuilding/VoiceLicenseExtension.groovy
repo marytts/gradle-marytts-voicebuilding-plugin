9da88646ebaa553319397b1fc12355d9fd17c416
Ingmar Steiner <steiner@coli.uni-saarland.de>
2016-01-03 20:17:47 +0100

    add voice license extension and unit tests

---
+++ base-plugin/src/main/groovy/de/dfki/mary/voicebuilding/VoiceLicenseExtension.groovy
@@ -0,0 +1,14 @@
+package de.dfki.mary.voicebuilding
+
+class VoiceLicenseExtension {
+
+    String name
+    String shortName
+    String url
+
+    VoiceLicenseExtension() {
+        name = 'Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International'
+        shortName = 'CC BY-NC-SA 4.0'
+        url = 'http://creativecommons.org/licenses/by-nc-sa/4.0/'
+    }
+}
