de6928d5b8e637f3f47ff9a90998d23ce1fce216
Ingmar Steiner <steiner@coli.uni-saarland.de>
2016-01-28 01:06:53 +0100

    adapt remainder of old LoadVoiceIT code

--- base-plugin/src/main/groovy/de/dfki/mary/voicebuilding/tasks/GenerateSource.groovy
+++ base-plugin/src/main/groovy/de/dfki/mary/voicebuilding/tasks/GenerateSource.groovy
@@ -113,25 +113,74 @@ class GenerateSource extends DefaultTask {
                                         """|package marytts.voice.${project.voice.nameCamelCase}
                                            |
                                            |import marytts.LocalMaryInterface
+                                           |import marytts.datatypes.MaryDataType
+                                           |import ${project.voice.type == 'hsmm' ? 'marytts.htsengine.HMMVoice' : 'marytts.unitselection.UnitSelectionVoice'}
+                                           |import marytts.util.dom.DomUtils
                                            |
                                            |import org.testng.annotations.*
                                            |
                                            |public class LoadVoiceIT {
                                            |
                                            |    LocalMaryInterface mary
+                                           |    Config config
                                            |
                                            |    @BeforeMethod
                                            |    void setup() {
                                            |        mary = new LocalMaryInterface()
+                                           |        config = new Config()
+                                           |    }
+                                           |
+                                           |    @Test
+                                           |    void canLoadVoice() {
+                                           |        def voice = new ${project.voice.type == 'hsmm' ? 'HMM' : 'UnitSelection'}Voice(config.name, null)
+                                           |        assert voice
                                            |    }
                                            |
                                            |    @Test
                                            |    void canSetVoice() {
-                                           |        def voiceName = new Config().name
-                                           |        mary.voice = voiceName
-                                           |        assert voiceName == mary.voice
+                                           |        mary.voice = config.name
+                                           |        assert config.name == mary.voice
                                            |    }
                                            |
+                                           |    @Test
+                                           |    void canProcessTextToTargetfeatures() {
+                                           |        mary.locale = config.locale
+                                           |        mary.outputType = MaryDataType.TARGETFEATURES
+                                           |        def input = MaryDataType.getExampleText(MaryDataType.TEXT, config.locale)
+                                           |        def output = mary.generateText(input)
+                                           |        assert output
+                                           |    }
+                                           |
+                                           |    @Test
+                                           |    void canProcessTextToSpeech() {
+                                           |        def mary = new LocalMaryInterface()
+                                           |        def config = new Config()
+                                           |        mary.voice = config.name
+                                           |        def input = MaryDataType.getExampleText(MaryDataType.TEXT, config.locale)
+                                           |        def output = mary.generateAudio(input)
+                                           |        assert output
+                                           |    }
+                                           |
+                                           |    @Test
+                                           |    void canProcessTokensToTargetfeatures() {
+                                           |        mary.locale = config.locale
+                                           |        mary.inputType = MaryDataType.TOKENS
+                                           |        mary.outputType = MaryDataType.TARGETFEATURES
+                                           |        def example = MaryDataType.getExampleText(MaryDataType.TOKENS, config.locale)
+                                           |        def input = DomUtils.parseDocument(example)
+                                           |        def output = mary.generateText(input)
+                                           |        assert output
+                                           |    }
+                                           |
+                                           |    @Test
+                                           |    void canProcessTokensToSpeech() {
+                                           |        mary.locale = config.locale
+                                           |        mary.inputType = MaryDataType.TOKENS
+                                           |        def example = MaryDataType.getExampleText(MaryDataType.TOKENS, config.locale)
+                                           |        def input = DomUtils.parseDocument(example)
+                                           |        def audio = mary.generateAudio(input)
+                                           |        assert output
+                                           |    }
                                            |}
                                            |""".stripMargin()
                                 )
