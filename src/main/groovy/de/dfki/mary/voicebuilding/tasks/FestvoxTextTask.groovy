9b93ea8381361d239eed371b9adc0cc42a5d3069
Ingmar Steiner <steiner@coli.uni-saarland.de>
2016-08-11 17:41:20 +0200

    add some logging to FestvoxTextTask

--- src/main/groovy/de/dfki/mary/voicebuilding/tasks/FestvoxTextTask.groovy
+++ src/main/groovy/de/dfki/mary/voicebuilding/tasks/FestvoxTextTask.groovy
@@ -17,11 +17,18 @@ class FestvoxTextTask extends DefaultTask {
     @TaskAction
     void extract() {
         def srcFile = project.file("$project.sourceSets.data.output.resourcesDir/$srcFileName")
+        def processed = 0
         srcFile.eachLine { line ->
             def m = line =~ /\( (?<utt>.+) "(?<text>.+)" \)/
             if (m.matches()) {
-                new File("$destDir/${m.group('utt')}.txt").text = m.group('text')
+                def destFile = new File("$destDir/${m.group('utt')}.txt")
+                project.logger.debug "Wrote $destFile"
+                destFile.text = m.group('text')
+                processed += 1
+            } else {
+                project.logger.warn "Could not process line:\n$line"
             }
         }
+        project.logger.info "Extracted $processed utterances into $destDir"
     }
 }
