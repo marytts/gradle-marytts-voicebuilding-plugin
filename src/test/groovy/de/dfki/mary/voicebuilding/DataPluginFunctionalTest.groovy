61fdccd5111062755e00a41fb0ff4cb4023f5f1e
Ingmar Steiner <steiner@coli.uni-saarland.de>
2017-07-20 14:42:01 +0200

    parameterize data plugin functional testing

--- src/test/groovy/de/dfki/mary/voicebuilding/DataPluginFunctionalTest.groovy
+++ src/test/groovy/de/dfki/mary/voicebuilding/DataPluginFunctionalTest.groovy
@@ -28,90 +28,33 @@ class DataPluginFunctionalTest {
         }
     }
 
-    @Test
-    void testHelp() {
-        def result = gradle.withArguments().build()
-        println result.output
-        assert result.task(':help').outcome == SUCCESS
+    @DataProvider
+    Object[][] taskNames() {
+        // task name to run, and whether to chase it with a test task named "testName"
+        [
+                ['help', false],
+                ['testConfigurations', false],
+                ['testSourceSets', false],
+                ['testDependencies', false],
+                ['praatPitchmarker', true],
+                ['mcepMaker', true],
+                ['generateAllophones', true],
+                ['generatePhoneFeatures', true],
+                ['generateHalfPhoneFeatures', true]
+        ]
     }
 
-    @Test
-    void testConfigurations() {
-        def result = gradle.withArguments('testConfigurations').build()
+    @Test(dataProvider = 'taskNames')
+    void testTasks(String taskName, boolean runTestTask) {
+        def result = gradle.withArguments(taskName).build()
         println result.output
-        assert result.task(':testConfigurations').outcome == SUCCESS
-    }
-
-    @Test
-    void testSourceSets() {
-        def result = gradle.withArguments('testSourceSets').build()
-        println result.output
-        assert result.task(':testSourceSets').outcome == SUCCESS
-    }
-
-    @Test
-    void testDependencies() {
-        def result = gradle.withArguments('testDependencies').build()
-        println result.output
-        assert result.task(':testDependencies').outcome == SUCCESS
-    }
-
-    @Test
-    void testPraatPitchmarker() {
-        def result = gradle.withArguments('praatPitchmarker').build()
-        println result.output
-        assert result.task(':wav').outcome in [SUCCESS, UP_TO_DATE]
-        assert result.task(':praatPitchmarker').outcome in [SUCCESS, UP_TO_DATE]
-        result = gradle.withArguments('testPraatPitchmarker').build()
-        println result.output
-        assert result.task(':wav').outcome == UP_TO_DATE
-        assert result.task(':praatPitchmarker').outcome == UP_TO_DATE
-        assert result.task(':testPraatPitchmarker').outcome == SUCCESS
-    }
-
-    @Test(dependsOnMethods = ['testPraatPitchmarker'])
-    void testMcepMaker() {
-        def result = gradle.withArguments('mcepMaker').build()
-        println result.output
-        assert result.task(':mcepMaker').outcome in [SUCCESS, UP_TO_DATE]
-        result = gradle.withArguments('testMcepMaker').build()
-        println result.output
-        assert result.task(':mcepMaker').outcome == UP_TO_DATE
-        assert result.task(':testMcepMaker').outcome == SUCCESS
-    }
-
-    @Test
-    void testGenerateAllophones() {
-        def result = gradle.withArguments('generateAllophones').build()
-        println result.output
-        assert result.task(':text').outcome in [SUCCESS, UP_TO_DATE]
-        assert result.task(':generateAllophones').outcome in [SUCCESS, UP_TO_DATE]
-        result = gradle.withArguments('testGenerateAllophones').build()
-        println result.output
-        assert result.task(':text').outcome == UP_TO_DATE
-        assert result.task(':generateAllophones').outcome == UP_TO_DATE
-        assert result.task(':testGenerateAllophones').outcome == SUCCESS
-    }
-
-    @Test(dependsOnMethods = ['testGenerateAllophones'])
-    void testGeneratePhoneFeatures() {
-        def result = gradle.withArguments('generatePhoneFeatures').build()
-        println result.output
-        assert result.task(':generatePhoneFeatures').outcome in [SUCCESS, UP_TO_DATE]
-        result = gradle.withArguments('testGeneratePhoneFeatures').build()
-        println result.output
-        assert result.task(':generatePhoneFeatures').outcome == UP_TO_DATE
-        assert result.task(':testGeneratePhoneFeatures').outcome == SUCCESS
-    }
-
-    @Test(dependsOnMethods = ['testGenerateAllophones'])
-    void testGenerateHalfPhoneFeatures() {
-        def result = gradle.withArguments('generateHalfPhoneFeatures').build()
-        println result.output
-        assert result.task(':generateHalfPhoneFeatures').outcome in [SUCCESS, UP_TO_DATE]
-        result = gradle.withArguments('testGenerateHalfPhoneFeatures').build()
-        println result.output
-        assert result.task(':generateHalfPhoneFeatures').outcome == UP_TO_DATE
-        assert result.task(':testGenerateHalfPhoneFeatures').outcome == SUCCESS
+        assert result.task(":$taskName").outcome in [SUCCESS, UP_TO_DATE]
+        if (runTestTask) {
+            def testTaskName = 'test' + taskName.capitalize()
+            result = gradle.withArguments(testTaskName).build()
+            println result.output
+            assert result.task(":$taskName").outcome == UP_TO_DATE
+            assert result.task(":$testTaskName").outcome == SUCCESS
+        }
     }
 }
