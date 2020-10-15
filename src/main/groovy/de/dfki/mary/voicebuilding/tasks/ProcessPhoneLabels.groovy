e036b8ce426851538901f5c265a396c1f02a5a01
Sebastien Le Maguer <sebastien@tcd.ie>
2020-10-15 09:03:30 +0200

    Fix features/units pause misalaignment

--- src/main/groovy/de/dfki/mary/voicebuilding/tasks/ProcessPhoneLabels.groovy
+++ src/main/groovy/de/dfki/mary/voicebuilding/tasks/ProcessPhoneLabels.groovy
@@ -28,17 +28,21 @@ class ProcessPhoneLabels extends DefaultTask {
         basenamesFile.get().asFile.eachLine('UTF-8') { basename ->
             def srcFile = srcDir.file("${basename}.lab").get().asFile
             def phoneTier = labSerializer.fromString(srcFile.getText('UTF-8')).tiers.first()
+            
+            // merge duplicate pauses
             def phoneIterator = phoneTier.annotations.listIterator()
             def phone = phoneIterator.next()
             while (phoneIterator.hasNext()) {
                 def nextPhone = phoneIterator.next()
-                // merge duplicate pauses
                 if (phone.text == nextPhone.text && phone.text == '_') {
                     phone.end = nextPhone.end
                     phoneIterator.remove()
                 }
+
                 phone = nextPhone
             }
+
+
             destDir.file("${basename}.lab").get().asFile.withWriter('UTF-8') { dest ->
                 def textGrid = new TextGrid(phoneTier.start, phoneTier.end, [phoneTier])
                 dest << labSerializer.toString(textGrid, tierName)
