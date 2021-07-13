7b084c8dda09c8a3b753c51d97e2c3f63e0672eb
Ingmar Steiner <steiner@coli.uni-saarland.de>
2021-07-13 08:06:36 +0200

    Add test for exclusion of basenames with missing files

--- src/test/groovy/de/dfki/mary/voicebuilding/tasks/GenerateBasenamesListTest.groovy
+++ src/test/groovy/de/dfki/mary/voicebuilding/tasks/GenerateBasenamesListTest.groovy
@@ -46,4 +46,47 @@ class GenerateBasenamesListFunctionalTest {
         def actual = new File(buildDir, 'basenames.lst').readLines()
         assert expected == actual
     }
+
+    @Test
+    void 'Given data directories, When files are missing, Then basenames excludes them'() {
+        def projectDir = File.createTempDir()
+        def buildDir = new File(projectDir, 'build')
+
+        def basenames = (1..5).collect { i ->
+            String.format('test_%04d', i)
+        }
+        ['wav', 'txt', 'lab'].each { dirName ->
+            def dir = new File(projectDir, dirName)
+            dir.mkdir()
+            basenames.each { basename ->
+                new File(dir, "${basename}.${dirName}").createNewFile()
+            }
+        }
+
+        new File(projectDir.path + File.separator + 'wav', 'test_0002.wav').delete()
+        new File(projectDir.path + File.separator + 'lab', 'test_0004.lab').delete()
+
+        new File(projectDir, 'build.gradle').text = '''
+            plugins {
+                id 'de.dfki.mary.voicebuilding-data'
+            }
+            
+            basenames {
+                wavDir = file('wav')
+                textDir = file('txt')
+                labDir = file('lab')
+            }
+            '''.stripMargin()
+
+        GradleRunner.create()
+                .withProjectDir(projectDir)
+                .withPluginClasspath()
+                .forwardOutput()
+                .withArguments(DEFAULT_ARGS + 'basenames')
+                .build()
+
+        def expected = basenames - ['test_0002', 'test_0004']
+        def actual = new File(buildDir, 'basenames.lst').readLines()
+        assert expected == actual
+    }
 }
