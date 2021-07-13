c3175f0f68be33ecf5f252fcbdf61a76f55e71c9
Ingmar Steiner <steiner@coli.uni-saarland.de>
2021-07-13 08:24:23 +0200

    Refactor to DRY

--- src/test/groovy/de/dfki/mary/voicebuilding/tasks/GenerateBasenamesListTest.groovy
+++ src/test/groovy/de/dfki/mary/voicebuilding/tasks/GenerateBasenamesListTest.groovy
@@ -1,60 +1,52 @@
 package de.dfki.mary.voicebuilding.tasks
 
+import org.gradle.testkit.runner.BuildResult
 import org.gradle.testkit.runner.GradleRunner
 import org.testng.annotations.Test
 
 class GenerateBasenamesListFunctionalTest {
 
-    final List DEFAULT_ARGS = ['--warning-mode', 'all', '--stacktrace', '--info']
-
     @Test
     void 'Given data directories, When basenames list is generated, Then basenames are in sort order'() {
         def projectDir = File.createTempDir()
-        def buildDir = new File(projectDir, 'build')
 
-        def basenames = (1..5).collect { i ->
-            String.format('test_%04d', i)
-        }
-        ['wav', 'txt', 'lab'].each { dirName ->
-            def dir = new File(projectDir, dirName)
-            dir.mkdir()
-            basenames.each { basename ->
-                new File(dir, "${basename}.${dirName}").createNewFile()
-            }
-        }
+        def basenames = generateBasenames(5)
+        createDataDirectories(projectDir, basenames)
 
-        new File(projectDir, 'build.gradle').text = '''
-            plugins {
-                id 'de.dfki.mary.voicebuilding-data'
-            }
-            
-            basenames {
-                wavDir = file('wav')
-                textDir = file('txt')
-                labDir = file('lab')
-            }
-            '''.stripMargin()
-
-        GradleRunner.create()
-                .withProjectDir(projectDir)
-                .withPluginClasspath()
-                .forwardOutput()
-                .withArguments(DEFAULT_ARGS + 'basenames')
-                .build()
+        generateBuildScript(projectDir)
+        runGradle(projectDir)
 
         def expected = basenames
-        def actual = new File(buildDir, 'basenames.lst').readLines()
+        def actual = new File(projectDir, 'basenames.lst').readLines()
         assert expected == actual
     }
 
     @Test
-    void 'Given data directories, When files are missing, Then basenames excludes them'() {
+    void 'Given data directories with some missing files, When basenames list is generated, Then basenames exclude them'() {
         def projectDir = File.createTempDir()
-        def buildDir = new File(projectDir, 'build')
 
-        def basenames = (1..5).collect { i ->
+        def basenames = generateBasenames(6)
+        createDataDirectories(projectDir, basenames)
+
+        new File(projectDir.path + File.separator + 'wav', 'test_0002.wav').delete()
+        new File(projectDir.path + File.separator + 'txt', 'test_0004.txt').delete()
+        new File(projectDir.path + File.separator + 'lab', 'test_0006.lab').delete()
+
+        generateBuildScript(projectDir)
+        runGradle(projectDir)
+
+        def expected = basenames - ['test_0002', 'test_0004', 'test_0006']
+        def actual = new File(projectDir, 'basenames.lst').readLines()
+        assert expected == actual
+    }
+
+    private static List<String> generateBasenames(int n) {
+        (1..n).collect { i ->
             String.format('test_%04d', i)
         }
+    }
+
+    private static List<String> createDataDirectories(projectDir, basenames) {
         ['wav', 'txt', 'lab'].each { dirName ->
             def dir = new File(projectDir, dirName)
             dir.mkdir()
@@ -62,10 +54,9 @@ class GenerateBasenamesListFunctionalTest {
                 new File(dir, "${basename}.${dirName}").createNewFile()
             }
         }
+    }
 
-        new File(projectDir.path + File.separator + 'wav', 'test_0002.wav').delete()
-        new File(projectDir.path + File.separator + 'lab', 'test_0004.lab').delete()
-
+    private static void generateBuildScript(File projectDir) {
         new File(projectDir, 'build.gradle').text = '''
             plugins {
                 id 'de.dfki.mary.voicebuilding-data'
@@ -75,18 +66,18 @@ class GenerateBasenamesListFunctionalTest {
                 wavDir = file('wav')
                 textDir = file('txt')
                 labDir = file('lab')
+                destFile = file('basenames.lst')
             }
             '''.stripMargin()
+    }
 
+    private static BuildResult runGradle(File projectDir) {
+        def defaultArgs = ['--warning-mode', 'all', '--stacktrace', '--info']
         GradleRunner.create()
                 .withProjectDir(projectDir)
                 .withPluginClasspath()
                 .forwardOutput()
-                .withArguments(DEFAULT_ARGS + 'basenames')
+                .withArguments(defaultArgs + 'basenames')
                 .build()
-
-        def expected = basenames - ['test_0002', 'test_0004']
-        def actual = new File(buildDir, 'basenames.lst').readLines()
-        assert expected == actual
     }
 }
